611e2819722c707723ea6865043b7d1200022729
Chris <gem6121@dingtalk.com>
2021-10-25 21:17:38 +0800

    git for hot-fix code change

--- src/main/java/com/acca/demo/DemoApplication.java
+++ src/main/java/com/acca/demo/DemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("git for hot-fix code change");
+		System.out.println("git for hot-fix code change2");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
