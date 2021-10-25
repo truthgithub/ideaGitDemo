4fd1fd57d52df5cd07b7d0e116604db9c2af4192
Chris <gem6121@dingtalk.com>
2021-10-25 21:19:35 +0800

    git for master code change

--- src/main/java/com/acca/demo/DemoApplication.java
+++ src/main/java/com/acca/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("git for master code change");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
